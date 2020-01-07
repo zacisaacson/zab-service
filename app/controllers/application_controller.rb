89203c637e3202288bed545572f6dd1d811461af
Laura Schulz <lrs8810@gmail.com>
2020-01-07 16:44:35 -0700

    Add accounts endpoint to ApplicationController
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Zac Isaacson <zacisaacson@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
+require 'faraday'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, "app/views"
     set :public_dir, "public"
   end
 
-  get '/' do
-    'Hello world!'
+  get '/api/v1/accounts/:token' do
+    Faraday.get("https://api.youneedabudget.com/v1/budgets/default/accounts?access_token=#{params[:token]}").body
   end
 end
